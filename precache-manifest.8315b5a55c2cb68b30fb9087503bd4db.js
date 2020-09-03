a4f4a3c99ece01f083fa10ff61306f08e2e829ec
TOR <52118178+fredzerpa@users.noreply.github.com>
2020-09-03 12:48:45 -0400

    Updates

---
+++ precache-manifest.8315b5a55c2cb68b30fb9087503bd4db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be361a5b559736bf7f748bc38d04aa31",
+    "url": "/iostronic/index.html"
+  },
+  {
+    "revision": "20579f9397d616c269dd",
+    "url": "/iostronic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c871a2cf45e8cc6c0ba2",
+    "url": "/iostronic/static/js/2.ef510c32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iostronic/static/js/2.ef510c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20579f9397d616c269dd",
+    "url": "/iostronic/static/js/main.4f5c44fa.chunk.js"
+  },
+  {
+    "revision": "bc279a69c97edf4a72c6",
+    "url": "/iostronic/static/js/runtime-main.15159a6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iostronic/static/media/logo.5d5d9eef.svg"
+  }
+]);
